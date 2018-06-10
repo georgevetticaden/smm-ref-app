027312f0db9d9cfa81b7ba439b905432b65439bd
georgevetticaden <george.vetticaden@gmail.com>
2018-06-10 05:35:29 -0700

    initial commit

---
+++ src/main/java/hortonworks/hdf/smm/refapp/consumer/impl/TruckGeoEventConsumer.java
@@ -0,0 +1,69 @@
+package hortonworks.hdf.smm.refapp.consumer.impl;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+
+import net.sourceforge.argparse4j.inf.ArgumentParser;
+import net.sourceforge.argparse4j.inf.ArgumentParserException;
+import net.sourceforge.argparse4j.inf.Namespace;
+
+import org.apache.avro.generic.GenericRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import hortonworks.hdf.smm.refapp.consumer.AbstractConsumeLoop;
+
+public class TruckGeoEventConsumer extends AbstractConsumeLoop<String, GenericRecord> {
+	
+	
+	private static final Logger logger = LoggerFactory.getLogger(TruckGeoEventConsumer.class);	
+
+	public TruckGeoEventConsumer(Properties configs, List<String> topics) {
+		super(configs, topics);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void process(ConsumerRecord<String, GenericRecord> record) {
+		
+		logger.info("C : {}, Record received partition : {}, key : {}, value : {}, offset : {}",
+				clientId, record.partition(), record.key(), record.value(), record.offset());
+		sleep(5000);
+		
+	}
+
+
+
+	public static void main(String[] args) throws InterruptedException {
+
+		ArgumentParser parser = argParser();
+
+		try {
+			Namespace result = parser.parseArgs(args);
+			List<String> topics = Arrays.asList(result.getString("topics").split(","));
+			Properties configs = getConsumerConfigs(result);
+
+			final TruckGeoEventConsumer consumer = new TruckGeoEventConsumer(configs, topics);
+			consumer.run();
+			
+			Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+				
+				@Override
+				public void run() {
+					logger.info("Shutting down consumer..");
+					consumer.close();
+				}
+			}));
+			
+		} catch (ArgumentParserException e) {
+			if(args.length == 0)
+				parser.printHelp();
+			else 
+				parser.handleError(e);
+			System.exit(0);
+		}
+	}	
+
+}
